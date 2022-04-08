27b9b893b45cf2a50b5273edcaa7e38070458186
ChCoque <Charles.Coque@cirquedusoleil.com>
2022-04-08 13:37:45 -0400

    add seed data

--- Violinews/Models/ViolinewsContext.cs
+++ Violinews/Models/ViolinewsContext.cs
@@ -4,11 +4,22 @@ namespace Violinews.Models
 {
     public class ViolinewsContext : DbContext
     {
+        public DbSet<Post> Posts { get; set; } = default!;
+
         public ViolinewsContext(DbContextOptions<ViolinewsContext> options) : base(options)
         { 
         }
 
 
-        public DbSet<Post> Posts { get; set; } = default!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>().HasData(new Post[]
+            {
+                new Post(Guid.NewGuid(), "Broke my E string", "so i was playing mendelssohn and i broke my E string", DateTime.Now.AddHours(-1)),
+                new Post(Guid.NewGuid(), "Got a new set of strings", "so i broke my E string but i got a new set of strings", DateTime.Now)
+            });
+    
+        }
+
     }
 }
